47eff1610cb8d4a91a847ad171c72aabf1519dc9
Anatol Pomozov <anatol.pomozov@gmail.com>
2026-03-25 07:49:22 -0700

    Add tests to the project

---
+++ BooklightTests/BooklightTestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  BooklightTestsLaunchTests.swift
+//  BooklightTests
+//
+//  Created by Anatol Pomozov on 3/25/26.
+//
+
+import XCTest
+
+final class BooklightTestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
